cb601e825063fcb8c8e49fbbac4a42906808bd1c
farlls <farrelshahizdn22@gmail.com>
2022-03-23 12:27:29 +0700

    Add project files.

---
+++ TaxiConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TaxiConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+            taxi.DriverName = "Farrel";
+            taxi.OnDuty = true;
+            taxi.NumPassangger = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassangger();
+            taxi.DropOffPassangger();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ TaxiConsoleApp/Taxi.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiConsoleApp
+{
+    class Taxi
+    {
+        public string DriverName { get; set; }
+        public int NumPassangger { get; set; }
+        public bool OnDuty { get; set; }
+           
+        public void TaxiInfo()
+        { 
+            string result = (OnDuty == true) ? "Yes" : "No";
+            Console.WriteLine("Nama Driver : {0}", DriverName);
+            Console.WriteLine("OnDuty : {0}", result);
+            Console.WriteLine("Number Of Passangger : {0}\n", NumPassangger);
+        }
+        public void PickUpPassangger()
+        {
+            Console.WriteLine("{0} Sedang Menjemput Penumpang", DriverName);
+        }
+        public void DropOffPassangger()
+        {
+            Console.WriteLine("{0} Selesai Mengantar Penumpang", DriverName);
+        }
+
+
+
+
+    }
+}
